81650d12af6f78293a2dc80f5d51b2e8f4f345ba
CleanSDT <148751998+CleanSDT@users.noreply.github.com>
2024-09-29 17:29:37 +0800

    Update loadNii.py

--- dataLoad/loadNii.py
+++ dataLoad/loadNii.py
@@ -1,16 +1,6 @@
 import SimpleITK as sitk
 import numpy as np
-def get_spinedata(dataDir, pedicle_points, pedicle_points_in_zyx, input_z=64, input_y=80, input_x=160):
-    """data_prepare
-
-    Args:
-        dataDir (str): nii路径
-        pedicle_points (np): 椎弓根中心点坐标
-        pedicle_points_in_zyx (bool): 坐标是否是zyx形式
-
-    Returns:
-        _type_: [掩模numpy, 各体素的坐标, 椎弓根中心点], 体素间距
-    """
+def get_spinedata(dataDir, pedicle_points, pedicle_points_in_zyx, input_z=, input_y=, input_x=):
     mask = sitk.ReadImage(dataDir)
     spacing = list(mask.GetSpacing())
     tmp = spacing[0]
@@ -18,7 +8,7 @@ def get_spinedata(dataDir, pedicle_points, pedicle_points_in_zyx, input_z=64, in
     spacing[2] = tmp
     mask_array = sitk.GetArrayFromImage(mask)
     mask_array, pedicle_points = resize(mask_array, pedicle_points, input_z, input_y, input_x)
-    if pedicle_points_in_zyx: #坐标是zyx形式,需要改成xyz形式
+    if pedicle_points_in_zyx: 
         temp = pedicle_points[:, 0].copy()
         pedicle_points[:, 0] = pedicle_points[:, 2]
         pedicle_points[:, 2] = temp
@@ -43,24 +33,24 @@ def resize(img, pts,input_s,input_h,input_w):
         img = resize_image_itk(sitk.GetImageFromArray(img), newSize=[input_w,input_h,input_s], resamplemethod=sitk.sitkLinear)
         return sitk.GetArrayFromImage(img), pts
 
-# simpleItk resize 三维ct图像
+
 def resize_image_itk(itkimage, newSize, resamplemethod=sitk.sitkNearestNeighbor):
     resampler = sitk.ResampleImageFilter()
     origin = itkimage.GetOrigin()
     #print('origin: ', origin)
-    originSize = itkimage.GetSize()  # 原来的体素块尺寸
+    originSize = itkimage.GetSize() 
     #print('originSize: ', originSize)
     originSpacing = itkimage.GetSpacing()
     #print('originSpacing: ',originSpacing)
     newSize = np.array(newSize, float)
     factor = originSize / newSize
     newSpacing = originSpacing * factor
-    newSize = newSize.astype(np.int32)  # spacing肯定不能是整数
-    resampler.SetReferenceImage(itkimage)   # 需要重新采样的目标图像
+    newSize = newSize.astype(np.int32) 
+    resampler.SetReferenceImage(itkimage) 
     resampler.SetSize(newSize.tolist())
     resampler.SetOutputSpacing(newSpacing.tolist())
     resampler.SetTransform(sitk.Transform(3, sitk.sitkIdentity))
     resampler.SetInterpolator(resamplemethod)
-    itkimgResampled = resampler.Execute(itkimage)  # 得到重新采样后的图像
+    itkimgResampled = resampler.Execute(itkimage)  
     #itkimgResampled = itkimgResampled.SetSpacing([1, 1, 1])
     return itkimgResampled
